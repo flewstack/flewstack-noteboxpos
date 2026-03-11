61ffaac3bf609413cd35f7bbd9e1929fc52f8bff
Flewstack <m.almousa@flewstack.com>
2026-03-11 14:13:02 +0300

    Updated flewstack_preview_documents

--- flewstack_preview_documents/static/src/js/report_preview_handler.js
+++ flewstack_preview_documents/static/src/js/report_preview_handler.js
@@ -6,13 +6,36 @@ import { user } from "@web/core/user";
 import { getReportUrl } from "@web/webclient/actions/reports/utils";
 import { FlewstackDocumentPreviewDialog } from "./report_preview_dialog";
 
-const SUPPORTED_MODELS = new Set(["account.move", "sale.order"]);
+const SUPPORTED_MODELS = new Set(["sale.order"]);
 const SUPPORTED_REPORT_NAMES = new Set([
     "account.report_invoice",
     "account.report_invoice_with_payments",
     "sale.report_saleorder",
     "sale.report_saleorder_pro_forma",
 ]);
+const ACCOUNT_INVOICE_REPORT_NAMES = new Set([
+    "account.report_invoice",
+    "account.report_invoice_with_payments",
+]);
+const ACCOUNT_INVOICE_MOVE_TYPES = new Set([
+    "out_invoice",
+    "out_refund",
+    "out_receipt",
+    "in_invoice",
+    "in_refund",
+    "in_receipt",
+]);
+
+function getActiveMoveIds(action) {
+    const activeIds = action.context?.active_ids;
+    if (Array.isArray(activeIds) && activeIds.length) {
+        return activeIds;
+    }
+    if (typeof action.context?.active_id === "number") {
+        return [action.context.active_id];
+    }
+    return [];
+}
 
 function shouldPreviewReport(action) {
     if (action.report_type !== "qweb-pdf") {
@@ -30,9 +53,35 @@ function shouldPreviewReport(action) {
     return SUPPORTED_REPORT_NAMES.has(action.report_name);
 }
 
+async function hasNonInvoiceMoves(action, env) {
+    const activeModel = action.context?.active_model || action.model;
+    if (activeModel !== "account.move") {
+        return false;
+    }
+    if (!ACCOUNT_INVOICE_REPORT_NAMES.has(action.report_name)) {
+        return false;
+    }
+
+    const ids = getActiveMoveIds(action);
+    if (!ids.length) {
+        return false;
+    }
+
+    const moves = await env.services.orm.read("account.move", ids, ["move_type"]);
+    return moves.some((move) => !ACCOUNT_INVOICE_MOVE_TYPES.has(move.move_type));
+}
+
 async function previewDocumentsHandler(action, options, env) {
     if (!shouldPreviewReport(action)) {
         return false;
+    }
+    if (await hasNonInvoiceMoves(action, env)) {
+        env.services.notification.add(
+            _t("Invoice PDF is only available for invoices and receipts."),
+            { type: "warning" }
+        );
+        options?.onClose?.();
+        return true;
     }
 
     const reportUrl = getReportUrl(action, "pdf", user.context);
