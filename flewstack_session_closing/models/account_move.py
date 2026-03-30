d1989367daa3ec8e259613bb25506e9ce42882b7
Flewstack <m.almousa@flewstack.com>
2026-03-30 10:12:00 +0300

    Updated flewstack_session_closing

--- flewstack_session_closing/models/account_move.py
+++ flewstack_session_closing/models/account_move.py
@@ -1,9 +1,26 @@
 from odoo import _, models
 from odoo.exceptions import UserError
+from odoo.addons.point_of_sale.models.account_move import AccountMove as PosAccountMove
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+    def button_draft(self):
+        blocked_orders = self.sudo().pos_order_ids.filtered(
+            lambda order: order.session_id.state not in ('closed', 'handed_over')
+        )
+        if blocked_orders:
+            self.env.user._bus_send("simple_notification", {
+                'type': 'danger',
+                'message': _(
+                    "You can't reset this invoice to draft because the POS session is still open.\n"
+                    "Please close the ongoing session first, then try again."
+                ),
+                'sticky': True,
+            })
+            return False
+        return super(PosAccountMove, self).button_draft()
 
     def action_open_transfer_cash(self):
         self.ensure_one()
